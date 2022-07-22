8b3d059cc692f37935d6d9977abbc33f356c9e09
Coolboy135 <Dreamz6489@outlook.com>
2022-07-23 00:44:33 +0200

    changes implemented

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import PEPO from './coolemoji.jpg';
+import './App.css';
+import LOL from './Coolirlboy.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header > 
+        <img src={PEPO} className="App-logo" alt="pepo" />
+        <p> A cool website Made by a cool boy. </p>
+        <img src={LOL} className="App-logo" alt="LOL" />
+        <p> Don ramon haha. </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
